c8cff2b47f04f42e22a695ad64092eb78468a4fa
ZachLTech <zlopezzl2021@gmail.com>
2024-12-22 15:11:04 -0500

    changed the response time

--- app/init.go
+++ app/init.go
@@ -6,7 +6,7 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-const responseTime = 5000 * time.Millisecond // The amount of milliseconds for the result to be displayed
+const responseTime = 8500 * time.Millisecond // The amount of milliseconds for the result to be displayed
 
 func (m Model) Init() tea.Cmd {
 	return nil
